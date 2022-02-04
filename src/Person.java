890c4334cb732b75049ea745109951af67ea3ce6
gerakanamalindonesia <gerakanamalindonesia@gmail.com>
2022-02-05 02:05:25 +0700

    membuat field/attribute dan mengaksesnya melalui objek

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,17 @@
 class Person {
-    // class diawali dengan Huruf capital (dan sesuai dengan nama file)
+    /**
+     * Field / Attribut / Properties
+     * Adalah data yang bisa kita sisipkan di dalam objek
+     * Namun sebelum kita bisa memasukkan data ke dalam object, terlebih dahulu kita deklarasikan data
+       apa saja yang dimiliki objek tersebut. Pendeklarasian ini kita buat di dalam class
+     * Membuat field itu sama seperti kita membuat variabel
+     */
+
+    String name;
+    String address;
+    final String ditrict = "Gunungkidul"; // kata kunci final artinya field tidak akan bisa diubah datanya (mirip kaya constanta)
+
+    /**
+     * Dengan membuat fild seperti di atas, maka class memiliki sesuatu utnuk diakses oleh objek-nya
+     */
 }
