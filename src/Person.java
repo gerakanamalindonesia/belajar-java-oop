14430209d95a762ee086577d259c27024a676878
gerakanamalindonesia <80880673+gerakanamalindonesia@users.noreply.github.com>
2022-02-05 15:51:11 +0700

    membuat constructor (#4)

--- src/Person.java
+++ src/Person.java
@@ -1,13 +1,21 @@
 class Person {
     /**
-     * Method
-     * Selain attribute, kita bisa menambahkan method ke dalam object
-     * Method itu bisa dibilang fungsi yang kita deklarasikan di class, kemudian bisa kita panggil pada saat membuat objek
+     * Constructor
+     * Constructor adalah method yang pertama kali dijalankan saat pertama kali object dibuat
+     * Mirip seperti di metod, kita bisa memberi parameter pada constructor
+     * Nama constructor harus sama dengan nama class, dan tidak membutuhkan kara kuci void serta tidak mengembalikan value
+     * Jika kita memberikan parameter di constructornya, maka saat membuat objek kita WAJIB mengisi parameternya ya
      */
 
     String name;
     String address;
     final String district = "Gunungkidul";
+
+    // membuat constructor
+    Person(String paramName, String paramAddres) {
+        name = paramName;
+        address = paramAddres;
+    }
 
     // membuat method
     void sayHello(String paramName) {
