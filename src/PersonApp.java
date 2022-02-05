ed553bf5cc6188e34b6aa504a749e0bd4c38c25a
gerakanamalindonesia <gerakanamalindonesia@gmail.com>
2022-02-05 15:49:39 +0700

    membuat constructor

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,10 +1,17 @@
 public class PersonApp {
     public static void main(String[] args) {
-        var person1 = new Person(); // cara pertama membuat object
-        person1.name = "Irwanto"; // menagakses field/attribute di class
-        person1.address = "Ciledug";
-        // person1.ditrict = "Bantul"; // jangan lakukan ini ya, karena pasti error (final tidak bisa diubah datanya)
+        var person1 = new Person("Irwanto", "Ciledug"); // "Irwanto" dan "Ciledug" adalah diberikan karena kita membuat constructor yang membutuhkan parameter name dan address
 
-        person1.sayHello("Rina Pratama");
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+
+        person1.sayHello("Rina");
+
+        Person person2 = new Person("Budi", "Jakarta");
+        person2.sayHello("Ani");
+
+        Person person3;
+        person3 = new Person("Joko", "Cirebon");
+        person3.sayHello("Marni");
     }
 }
