44e8eb6221945fc19db1ae20ec0ca982da26c130
gerakanamalindonesia <gerakanamalindonesia@gmail.com>
2022-02-07 11:07:32 +0700

    variabel shadowing

--- src/Person.java
+++ src/Person.java
@@ -1,20 +1,29 @@
 class Person {
     /**
-     * Constructor Overloading
-     * Sama seperti di method / function, kita bisa melakukan overloading
-     * Kita bisa membuat constructor lebih dari satu, dengan syarat tipe data parameter harus berbeda, atau jumlah parameter harus berbeda
-     */
-
-    /**
-     * Sama juga seperti di function, coinstructor bisa memanggil constructor lain
-     * Hal ini memudahkan saat kita butuh menginisialisasi data degan berbagai kemungkinan
-     * Cara memanggil constructor lain, kita hanya perlu memanggilnya seperti memanggil method, namun dengan kata kunci this
+     * Variabek Shadowing
+     * Variabel shadowing adalah kejadian ketika kita membuat
+       nama variabel dengan nama yang sama di scope yang menutupi
+       variabel dengan nama yang sama di scope di atasnya
+     * Ini biasa terjadi seperti kita membuat nama parameter di method
+       sama dengan nama field di class
+     * Saat terjadi variabel shadowing maka secara otomatis variabel
+       di atasnya tidak bisa diakses
      */
 
     String name;
     String address;
     final String district = "Gunungkidul";
 
+    Person(String name, String address) {
+        name = name; // name di ( = name ) ini ngambil di parameter, bukan di field si atasnya, sehingga name tidak akan berubah
+        address = address;
+    }
+
+    void sayHello(String name) {
+        System.out.println("Hello " + name + ", nama saya adalah " + name + ". Saya beralamat di " + address + ", dan serasal dari " + district);
+    }
+
+    /*
     // membuat constructor
     Person(String paramName, String paramAddres) {
         name = paramName;
@@ -30,9 +39,11 @@ class Person {
         this(null); // ini memanggi constructor Person di atas, dan karena di constructor ini tidak ada param name maka kita buat parameternya null
     }
 
+
     // membuat method
     void sayHello(String paramName) {
-        System.out.println("Hello " + paramName + ", nama saya adalah " + name + ". Saya beralamat di " + address + ", dan saya berasal dari " + district);
+        System.out.println("Hello " + paramName + ", nama saya adalah " + name + ". Saya beralamat di " + address + ", dan serasal dari " + district);
     }
+    */
 
 }
