d3bb7da24c2fac6302c0e54daf324b8aa4087e75
gerakanamalindonesia <gerakanamalindonesia@gmail.com>
2022-02-13 00:34:14 +0700

    membahas this dan kegunaannya

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,21 +1,19 @@
 public class PersonApp {
     public static void main(String[] args) {
-        var person1 = new Person("Irwanto", "Ciledug"); // constructor dua parameter
+        var person1 = new Person("Irwanto", "Ciledug");
+        /**
+         * Di atas kita membuaty object dari class Person
+         * Di dalam class person kita memberi nilai constructor yaitu Irwanto dan Ciledug
+         * Jika diliat constractor Person memerlukan parameter name dan address, sehingga Irwanto dan Ciledug akan mengisi itu
+         * Karena nama field dan name parameternya sama, maka untuk memberitahu bahwa field name sama dengan parameter name dipakailah this di situ
+         */
 
         System.out.println(person1.name);
         System.out.println(person1.address);
         person1.sayHello("Rina");
 
-        Person person2 = new Person("Budi", "Jakarta"); // constructor satu parameter
-        person2.address = "Jakarta";
+        Person person2 = new Person("Budi", "Jakarta");
+        person2.address = "Bandung"; // ini akan me-replace constructor "Jakarta" menjadi "Bandung"
         person2.sayHello("Ani");
-
-        /*
-        Person person3;
-        person3 = new Person(); // constuctor tanpa parameter
-        person3.name = "Joko";
-        person3.address = "Cirebon";
-        person3.sayHello("Marni");
-        */
     }
 }
