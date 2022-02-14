490141c5eb62cd1639e106654555f8c9a9adb900
gerakanamalindonesia <80880673+gerakanamalindonesia@users.noreply.github.com>
2022-02-14 23:36:17 +0700

    memahami method overriding (#9)

--- src/MobilApp.java
+++ src/MobilApp.java
@@ -8,6 +8,11 @@ public class MobilApp {
         // membuat object dari class Mobil (child class) yang merupakan turunan dari class Kendaraan
         var mobil = new Mobil();
         mobil.name = "Alphard";
-        mobil.sayMerk("Rina");
+        mobil.sayMerk("Rina"); // karena sayMerk didefinisikan ulang di child, maka method child-lah yang akan dipakai
+
+        // membuat object dari child class
+        var motor = new Motor();
+        motor.name = "Vario";
+        motor.sayMerk("Pukis"); // karena sayMerk didefinisikan ulang di child, maka method child-lah yang akan dipakai
     }
 }
---
+++ src/Motor.java
@@ -0,0 +1,5 @@
+public class Motor extends Kendaraan {
+    void sayMerk(String name) {
+        System.out.println("Halo " + name + ", ini adalah kendaraan merk " + this.name);
+    }
+}
