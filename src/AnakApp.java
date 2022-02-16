d9638d62072ea99c45fec3cf0758f6f92bcfb552
gerakanamalindonesia <80880673+gerakanamalindonesia@users.noreply.github.com>
2022-02-16 22:28:35 +0700

    memahami super constructor (#11)

---
+++ src/AnakApp.java
@@ -0,0 +1,9 @@
+public class AnakApp {
+    public static void main(String[] args) {
+        var anak = new Anak("Irwanto", "Jogja");
+        anak.sayAnak("Dini");
+
+        var anak2 = new Anak("Jono", "Banyumas");
+        anak2.greeting("Rini");
+    }
+}
